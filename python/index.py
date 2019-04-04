7dbff3649506c668336008c1a938abe77b7809f7
Duggal <duggaltanvir@gmail.com>
2019-04-03 17:41:41 -0700

    PyPDF2
    Data extracted from pdf and is displayed in console,
    Use of PyPDF2 and not tabula because of tabula dependencies on JAVA

--- python/index.py
+++ python/index.py
@@ -5,11 +5,19 @@ Created on Wed Apr  3 16:41:07 2019
 @author: Duggal
 """
 
-import tabula
+import PyPDF2
 
 def Main():
-    df = tabula.read_pdf("Sample/sample.pdf")
-    print(df)
-
+    pdfFile   = open('Sample/sample.pdf', 'rb')
+    
+    pdfReader = PyPDF2.PdfFileReader(pdfFile)
+    print("Number of Pages : " + str(pdfReader.numPages))
+    
+    for pagesInfo in range(pdfReader.numPages):
+        pageObj = pdfReader.getPage(pagesInfo)
+        print(">>>>> " + pageObj.extractText())
+    
+    pdfFile.close()
+    
 if __name__ == '__main__':
     Main()
