cff307c51a5214196a3b75a5d31d8842d8bc54ff
Duggal <duggaltanvir@gmail.com>
2019-04-09 17:47:51 -0700

    Extracting Emails and Links from PDF content

--- python/index.py
+++ python/index.py
@@ -6,18 +6,84 @@ Created on Wed Apr  3 16:41:07 2019
 """
 
 import PyPDF2
+import re
+
+class GetPDF:
+    pdfPath   = ""
+    pdf       = ""
+    numPages  = 0
+    content   = []
+    links     = []
+    phonenum  = []
+    email     = [] 
+    
+    def __init__(self, pdfPath):
+        self.pdfPath = pdfPath
+#        -------------- READING AND SETTING VALUES
+        with open(pdfPath,'rb') as pdfObj:
+            pdfReader = PyPDF2.PdfFileReader(pdfObj)
+            self.setNumPages(pdfReader.numPages)
+            
+            for i in range(self.getNumPages()):
+                self.setContent(pdfReader.getPage(i).extractText())
+                
+            self.extractText()
+       
+#        ------------------ EXTRACTING TEXT FROM PDF
+        
+    def extractText(self):
+        lines = self.content[0]
+        lines = lines.split('\n')
+        for i in lines:
+            if len(self.FindEmail(i)) != 0:
+                self.setemail(i)
+            
+            if len(self.FindURL(i)) != 0:
+                self.setLinks(i)
+                
+        print(self.getemail())
+        print(self.getLink())
+    
+#    ---------------- REGULAR EXPRESSSION FOR URL AND EMAIL
+        
+    def FindURL(self, string):  
+        url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string) 
+        return url 
+    
+    def FindEmail(self, string):
+        emails = re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", string)
+        return emails
+    
+#    -------------- GETTER SETTER OF CLASS
+    def setNumPages(self, numPages):
+        self.numPages = numPages
+    
+    def getNumPages(self):
+        return self.numPages
+    
+    def setContent(self, content):
+        self.content.append(content)
+    
+    def getContent(self):
+        return self.content
+    
+    def setLinks(self, link):
+        self.links.append(link)
+    
+    def getLink(self):
+        return self.links
+    
+    def setemail(self, email):
+        self.email.append(email)
+    
+    def getemail(self):
+        return self.email
 
 def Main():
-    pdfFile   = open('Sample/sample.pdf', 'rb')
-    
-    pdfReader = PyPDF2.PdfFileReader(pdfFile)
-    print("Number of Pages : " + str(pdfReader.numPages))
-    
-    for pagesInfo in range(pdfReader.numPages):
-        pageObj = pdfReader.getPage(pagesInfo)
-        print(">>>>> " + pageObj.extractText())
-    
-    pdfFile.close()
+    pdfPath = "Sample/sample4.pdf"
+     
+    pdfObj = GetPDF(pdfPath)
+
     
 if __name__ == '__main__':
     Main()
